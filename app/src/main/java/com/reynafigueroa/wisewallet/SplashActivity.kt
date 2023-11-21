e7ced75d6630fb878922c2145b8b6e017acdf961
Reyna Figueroa <reyalefi8@gmail.com>
2023-11-20 18:10:18 -0700

    Se creo el Splash Screen

---
+++ app/src/main/java/com/reynafigueroa/wisewallet/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.reynafigueroa.wisewallet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val splash = findViewById<ImageView>(R.id.splash)
+
+        splash.setOnClickListener(){
+
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+
+            finish()
+        }
+
+
+    }
+}
